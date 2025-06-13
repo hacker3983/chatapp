fd064de62e75df13cb3b6cb5016e5e0caea4ec19
hacker3983 <mrgamer19554@gmail.com>
2025-06-12 20:06:21 -0400

    Added message list types

---
+++ message_listdef.h
@@ -0,0 +1,27 @@
+#ifndef _MESSAGE_LISTDEF_H
+#define _MESSAGE_LISTDEF_H
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+
+typedef struct message {
+    TTF_Font* font;
+    int font_size;
+
+    // Our actual message properties
+    char* text;
+    SDL_Rect text_canvas;
+    SDL_Color text_color;
+
+    // This will be our background for our message
+    int padding_x, padding_y,
+        space_y;
+    SDL_Rect canvas;
+    SDL_Color canvas_color;
+} message_t;
+
+typedef struct message_list {
+    int start_x, start_y;
+    message_t* messages;
+    size_t message_count;
+} message_list_t;
+#endif
