25a300f256e03b4324bcb08dc5c0b4c1d5ddbd07
hacker3983 <mrgamer19554@gmail.com>
2025-06-12 20:05:11 -0400

    Added app configuration header file

---
+++ appconfig.h
@@ -0,0 +1,27 @@
+#ifndef _APPCONFIG_H
+#define _APPCONFIG_H
+#define WINDOW_WIDTH 900
+#define WINDOW_HEIGHT 600
+#define FONT_SIZE 36
+const char* header_text = "Chat Application in C";
+SDL_Color WINDOW_COLOR = {255, 255, 255, 255};
+SDL_Rect inputbox = {
+		.x = 0,
+		.y = 0,
+		.w = 100,
+		.h = 50
+};
+SDL_Rect inputbox_textcanvas = {0};
+SDL_Color inputbox_textcolor = {0xff, 0xff, 0xff, 0xff};
+SDL_Texture* inputbox_texture = NULL;
+SDL_Color inputbox_color = {0, 0, 0, 255};
+int header_w = 0, header_h = 0;
+char* message_buffer = NULL;
+SDL_Color header_textcolor = {255, 255, 0, 255};
+
+SDL_Texture* message_texture = NULL, *header_texture = NULL;
+SDL_Color message_color = {0, 0xff, 0x0, 0xff},
+		message_canvascolor = {0};
+bool enter_pressed = false,
+	display_message = false;
+#endif
