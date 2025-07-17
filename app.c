2c1467843fc44c7d0411bab9a55d0e1a27bf6e26
hacker3983 <mrgamer19554@gmail.com>
2025-07-17 19:35:48 -0400

    Fix spacing issues between vscode and vim

--- app.c
+++ app.c
@@ -10,28 +10,28 @@ bool received_message = false,
      recv_threadstarted = false;
 
 void app_init() {
-    if(SDL_Init(SDL_INIT_VIDEO) < 0) {
-        fprintf(stderr, "Failed to initialize SDL\n");
-    }
+	if(SDL_Init(SDL_INIT_VIDEO) < 0) {
+		fprintf(stderr, "Failed to initialize SDL\n");
+	}
 	if(TTF_Init() < 0) {
-        fprintf(stderr, "Failed to initialize SDL ttf\n");
-    }
+		fprintf(stderr, "Failed to initialize SDL ttf\n");
+	}
 	chatsock_init();
 }
 
 void app_create(app_t* app) {
-    app_init();
+	app_init();
 	app->server_sock = chatsock_create("127.0.0.1", PORT);
 	if(chatsock_connect(&app->server_sock) < 0) {
 		fprintf(stderr, "Failed to connect to the server\n");
 	} else {
 		printf("Connected to the server successfully!\n");
 	}
-    app->window = SDL_CreateWindow("Chat Application in C",
-			SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-			WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_RESIZABLE);
+	app->window = SDL_CreateWindow("Chat Application in C",
+		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+		WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_RESIZABLE);
 	app->renderer = SDL_CreateRenderer(app->window, -1, SDL_RENDERER_ACCELERATED);
-    app->font = TTF_OpenFont("Roboto-Italic-VariableFont_wdth,wght.ttf",
+	app->font = TTF_OpenFont("Roboto-Italic-VariableFont_wdth,wght.ttf",
 		FONT_SIZE);
 	app->font_size = FONT_SIZE;
 	app->inputbox_font = TTF_OpenFont("Symbola.ttf",
@@ -39,7 +39,7 @@ void app_create(app_t* app) {
 }
 
 void app_getwindowsize(app_t* app) {
-    SDL_GetWindowSize(app->window, &app->win_width, &app->win_height);
+	SDL_GetWindowSize(app->window, &app->win_width, &app->win_height);
 }
 
 void app_setwindowcolor(app_t* app, SDL_Color window_color) {
@@ -59,40 +59,40 @@ void* app_recv_message(void* arg) {
 
 void app_run(app_t* app) {
 	pthread_t recv_thread;
-    if(!app->font) {
-        printf("Failed to open font\n");
-    }
-    if(TTF_SizeText(app->font, header_text, &header_w, &header_h) < 0) {
-        printf("Failed to get the size of the text\n");
-    }
-    SDL_Rect header_textcanvas = {
+	if(!app->font) {
+		printf("Failed to open font\n");
+	}
+	if(TTF_SizeText(app->font, header_text, &header_w, &header_h) < 0) {
+		printf("Failed to get the size of the text\n");
+	}
+	SDL_Rect header_textcanvas = {
 		.x = 0,
 		.y = 0,
 		.w = (const int)header_w,
 		.h = (const int)header_h
-    };
-    SDL_Rect header = {
+	};
+	SDL_Rect header = {
 		.x = 0,
 		.y = 0,
 		.w = (const int)header_w + 40,
 		.h = (const int)header_h + 40
-    };
-    SDL_Color header_color = {0};
-
-    SDL_Rect message_canvas = {
-	    .x = 0,
+	};
+	SDL_Color header_color = {0};
+	
+	SDL_Rect message_canvas = {
+		.x = 0,
 		.y = header.y + header.h + 10,
 		.w = 0,
 		.h = 0
-    };
-    SDL_Surface* header_textsurface = TTF_RenderText_Blended(app->font, header_text,
-	    	header_textcolor);
-    if(header_textsurface) {
-        printf("The header textsurface is not NULL\n");
-    }
-    header_texture = SDL_CreateTextureFromSurface(app->renderer, header_textsurface);
+	};
+	SDL_Surface* header_textsurface = TTF_RenderText_Blended(app->font, header_text,
+		header_textcolor);
+	if(header_textsurface) {
+		printf("The header textsurface is not NULL\n");
+	}
+	header_texture = SDL_CreateTextureFromSurface(app->renderer, header_textsurface);
 	SDL_FreeSurface(header_textsurface);
-    while(!app->quit) {
+	while(!app->quit) {
 		while(SDL_PollEvent(&app->e)) {
 			if(app->e.type == SDL_QUIT) {
 				app->quit = true;
@@ -102,17 +102,17 @@ void app_run(app_t* app) {
 				inputbox_handle_events(app, &app->inputbox);
 			}
 		}
-        app_getwindowsize(app);
-        app_setwindowcolor(app, WINDOW_COLOR);
+		app_getwindowsize(app);
+		app_setwindowcolor(app, WINDOW_COLOR);
 		SDL_SetRenderDrawColor(app->renderer, color_toparam(header_color));
 		header.x = (app->win_width - header.w) / 2;
 		SDL_RenderDrawRect(app->renderer, &header);
 		SDL_RenderFillRect(app->renderer, &header);
 		header_textcanvas.x = header.x + (header.w - header_textcanvas.w) / 2;
 		header_textcanvas.y = header.y + (header.h - header_textcanvas.h) / 2;
-        if(header_texture) {
-            SDL_RenderCopy(app->renderer, header_texture, NULL, &header_textcanvas);
-        }
+		if(header_texture) {
+			SDL_RenderCopy(app->renderer, header_texture, NULL, &header_textcanvas);
+		}
 
 		if(!recv_threadstarted) {
 			pthread_create(&recv_thread, NULL, &app_recv_message, app);
@@ -149,9 +149,9 @@ void app_run(app_t* app) {
 		}
 		int start_x = 10, start_y = header.y + header.h + 10;
 		message_list_setstartpos(&app->message_list, start_x, start_y);
-    	// Display our message
+		// Display our message
 		message_list_display(app, &app->message_list);
-
+		
 		inputbox_init(&app->inputbox, app->inputbox_font, app->font_size,
 			inputbox_textcolor,
 			app->win_width - 40, 50, inputbox_color,
@@ -173,7 +173,7 @@ void app_destroy(app_t* app) {
 	message_list_destroy(&app->message_list);
 	inputbox_destroy(&app->inputbox);
 	SDL_DestroyTexture(header_texture);
-    SDL_DestroyRenderer(app->renderer);
+	SDL_DestroyRenderer(app->renderer);
 	SDL_DestroyWindow(app->window);
 	TTF_CloseFont(app->font);
 	TTF_CloseFont(app->inputbox_font);
